2868345c93a142e4a0387e997d527646896812b5
kaestel <martin@think.dk>
2015-04-09 21:02:49 +0200

    Making sure image captions are visible

--- src/templates/pages/page.php
+++ src/templates/pages/page.php
@@ -18,7 +18,9 @@ $this->pageTitle($item["name"]);
 	<div class="article i:article" itemscope itemtype="http://schema.org/Article">
 
 <?		if($media): ?>
-		<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
+		<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>">
+			<p>Image: <a href="/images/<?= $item["item_id"] ?>/<?= $media["variant"] ?>/500x.<?= $media["format"] ?>"><?= $media["name"] ?></a></p>
+		</div>
 <?		endif; ?>
 
 		<h1><?= $item["name"] ?></h1>
@@ -33,8 +35,10 @@ $this->pageTitle($item["name"]);
 
 <?		if($item["mediae"]):
 			foreach($item["mediae"] as $media): ?>
-		<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
-<? 			endforeach;
+		<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>">
+			<p>Image: <a href="/images/<?= $item["item_id"] ?>/<?= $media["variant"] ?>/500x.<?= $media["format"] ?>"><?= $media["name"] ?></a></p>
+		</div>
+<?			endforeach;
 		endif; ?>
 
 	</div>
--- src/templates/pages/posts.php
+++ src/templates/pages/posts.php
@@ -39,7 +39,9 @@ $pagination = $IC->paginate(array("pattern" => $pattern, "sindex" => $sindex, "l
 		<li class="item post id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/Article">
 
 <?			if($media): ?>
-			<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
+			<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>">
+				<p>Image: <a href="/images/<?= $item["item_id"] ?>/<?= $media["variant"] ?>/500x.<?= $media["format"] ?>"><?= $media["name"] ?></a></p>
+			</div>
 <?			endif; ?>
 
 			<ul class="tags">
@@ -70,8 +72,10 @@ $pagination = $IC->paginate(array("pattern" => $pattern, "sindex" => $sindex, "l
 
 <?			if($item["mediae"]):
 				foreach($item["mediae"] as $media): ?>
-			<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
-<? 				endforeach;
+			<div class="image item_id:<?= $item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>">
+				<p>Image: <a href="/images/<?= $item["item_id"] ?>/<?= $media["variant"] ?>/500x.<?= $media["format"] ?>"><?= $media["name"] ?></a></p>
+			</div>
+<?				endforeach;
 			endif; ?>
 
 		</li>
