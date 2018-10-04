4b3579eba4ffd9e5af96a07b13c3091e533672e9
Threepwood <scope.dk@gmail.com>
2018-10-04 16:47:48 +0200

    Update path js include

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,4 +1,4 @@
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/lib/seg_desktop_include.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/seg_desktop.js"></script>');
 
 // document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/manipulator/src/u-googleanalytics.js"></script>');
