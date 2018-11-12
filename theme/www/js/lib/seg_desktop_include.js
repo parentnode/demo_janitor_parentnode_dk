974eb27e7b449a653cdda7576edccadfadca31af
Threepwood <scope.dk@gmail.com>
2018-11-12 15:32:49 +0100

    Update desktop and desktop_light includes

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,7 +1,7 @@
 document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/seg_desktop.js"></script>');
 
 // document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
-document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/u-googleanalytics.js"></script>');
+// document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/u-googleanalytics.js"></script>');
 
 // document.write('<script type="text/javascript" src="/js/lib/desktop/i-page.js"></script>');
 // document.write('<script type="text/javascript" src="/js/lib/desktop/i-login.js"></script>');
