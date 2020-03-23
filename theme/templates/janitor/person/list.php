6b73e0f59101ac76b0ae33d3776159decb3294ae
Martin Kæstel Nielsen <martin@think.dk>
2020-03-23 12:14:24 +0100

    Update for janitor

--- theme/templates/janitor/person/list.php
+++ theme/templates/janitor/person/list.php
@@ -13,11 +13,11 @@ $items = $IC->getItems(array("itemtype" => $itemtype, "order" => "position ASC, 
 		<?= $JML->listNew(array("label" => "New person")) ?>
 	</ul>
 
-	<div class="all_items i:defaultList taggable filters sortable"<?= $JML->jsData() ?>>
+	<div class="all_items i:defaultList taggable filters sortable"<?= $HTML->jsData() ?>>
 <?		if($items): ?>
 		<ul class="items">
 <?			foreach($items as $item): ?>
-			<li class="item image draggable item_id:<?= $item["id"] ?> width:160<?= $JML->jsMedia($item, "single_media") ?>">
+			<li class="item image draggable item_id:<?= $item["id"] ?> width:160<?= $HTML->jsMedia($item, "single_media") ?>">
 				<div class="drag"></div>
 				<h3><?= $item["name"] ?></h3>
 
